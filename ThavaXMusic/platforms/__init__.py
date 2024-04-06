3d374cc5ae148c067e432621e8e25e08b5a03e73
campers98 <140605897+campers98@users.noreply.github.com>
2024-04-06 21:56:39 +0530

    Update __init__.py

--- ThavaXMusic/platforms/__init__.py
+++ ThavaXMusic/platforms/__init__.py
@@ -1,6 +1,5 @@
 from .Carbon import CarbonAPI
 from .Resso import RessoAPI
-from .Soundcloud import SoundAPI
 from .Spotify import SpotifyAPI
 from .Telegram import TeleAPI
 from .Youtube import YouTubeAPI
